bb77c1327aecdef448ef12f87b565413ebaea635
ArioNino <aryoelnino@gmail.com>
2025-05-05 23:40:52 +0700

    fix up foto produk

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\File;
 use App\Models\Produk;
 use Illuminate\Http\Request;
 
@@ -23,43 +23,79 @@ class ProdukController extends Controller
     return response()->json($produk);
     }
 
-    public function store(Request $request)
-    {
-        $validated = $request->validate([
-            'nama_produk' => 'required|string|max:255',
-            'kategori' => 'required|string|max:255',
-            'ukuran' => 'required|string|max:255',
-            'deskripsi' => 'required|string',
-            'harga' => 'required|numeric',
-            'gambar' => 'nullable|string',
-            'stok' => 'required|integer',
-        ]);
+    public function store(Request $request){
+            $data = $request->validate([
+                'nama_produk' => 'required',
+                'kategori' => 'required',
+                'deskripsi' => 'required',
+                'ukuran' => 'required',
+                'harga' => 'required',
+                'stok' => 'required|integer|min:0'
+            ]);
 
-        $produk = Produk::create($validated);
-        return response()->json($produk, 201);
+            $data = Produk::create([
+                'nama_produk' => $request->nama_produk,
+                'kategori' => $request->kategori,
+                'deskripsi' => $request->deskripsi,
+                'ukuran' => $request->ukuran,
+                'harga' => $request->harga,
+                'gambar' => null,
+                'stok' => $request->stok
+            ]);
+
+            if($request->hasFile('gambar')){
+                $file = $request->file('gambar');
+                $fileName = $this->quickRandom().$data->produk_id.'.'.$file->extension();
+                $path = $file->storeAs('produk', $fileName, 'public');
+                $data->update([
+                    'gambar' => $path
+                ]);
+            }
+
+            return response()->json([
+                'message' => 'Produk berhasil ditambah'
+            ]);
     }
 
-    public function update(Request $request, $id)
+    public static function quickRandom($length = 16)
     {
-        $produk = Produk::find($id);
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return substr(str_shuffle(str_repeat($pool, 5)), 0, $length);
+    }
 
-        if (!$produk) {
-            return response()->json(['message' => 'Produk tidak ditemukan'], 404);
-        }
-
-        $request->validate([
-            'nama_produk' => 'required|string|max:255',
-            'kategori' => 'required|string|max:255',
-            'ukuran' => 'required|string|max:255',
-            'deskripsi' => 'nullable|string',
-            'harga' => 'required|numeric',
-            'gambar' => 'nullable|string',
-            'stok' => 'required|integer'
+    public function update(Request $request, $id){
+        $produk = Produk::findOrFail($id);
+        $data = $request->validate([
+            'nama_produk' => 'required',
+            'kategori' => 'required',
+            'deskripsi' => 'required',
+            'ukuran' => 'required',
+            'harga' => 'required',
+            'stok' => 'required|integer|min:0'
         ]);
-
-        $produk->update($request->all());
-
-        return response()->json($produk);
+        if($request->hasFile('gambar')){
+            $pathLama = storage_path('app/public/'.$produk->gambar);
+            if(File::exists($pathLama)){
+                File::delete($pathLama);
+                $file = $request->file('gambar');
+                $fileName = $this->quickRandom().'.'.$file->extension();
+                $path = $file->storeAs('produk', $fileName, 'public');
+                $produk->update([
+                    'gambar' => $path
+                ]);
+            }
+        }
+        $produk->update([
+            'nama_produk' => $request->nama_produk,
+            'kategori' => $request->kategori,
+            'deskripsi' => $request->deskripsi,
+            'ukuran' => $request->ukuran,
+            'harga' => $request->harga,
+            'stok' => $request->stok
+        ]);
+        return response()->json([
+            'message' => 'Produk berhasil diperbarui'
+        ]);
     }
 
     public function destroy($id)
